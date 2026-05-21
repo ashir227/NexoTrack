bbe66f918766ebf07793530b94c0028be1643dd8
Ashir Ahmed <ashirahmed810@gmail.com>
2026-05-21 23:41:45 +0500

    NexoTranck Api

--- nexotrack/lib/Provider/ApiPro.dart
+++ nexotrack/lib/Provider/ApiPro.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/widgets.dart';
+import 'package:nexotrack/Models/ApiModel.dart';
 
-class ClassName extends ChangeNotifier {}
+class ClassName extends ChangeNotifier {
+  CryptoModel? user;
+}
