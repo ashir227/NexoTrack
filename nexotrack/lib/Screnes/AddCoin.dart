e89619f8db13479bb5c58fc4569ff22415b8d8d4
ashir227 <153205690+ashir227@users.noreply.github.com>
2026-05-25 21:09:08 +0500

    Add UI

--- nexotrack/lib/Screnes/AddCoin.dart
+++ nexotrack/lib/Screnes/AddCoin.dart
@@ -6,14 +6,22 @@ class Addcoin extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double w = MediaQuery.of(context).size.width;
+    double h = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: PrimaryColor.BckColor,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
+          SizedBox(height: h * 0.07),
           Text(
             "Add Cryptocurrency",
-            style: TextStyle(color: SecColor.textwhclr),
+
+            style: TextStyle(
+              color: SecColor.textwhclr,
+              fontSize: w * 0.08,
+              fontWeight: FontWeight.w700,
+            ),
           ),
         ],
       ),
