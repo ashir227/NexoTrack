8fba3ffa2463fb91faa6a2dba66b505fc2c1ff67
ashir227 <153205690+ashir227@users.noreply.github.com>
2026-05-26 13:30:03 +0500

    Coin Dropdown

--- nexotrack/lib/Core/widgets/drop_down.dart
+++ nexotrack/lib/Core/widgets/drop_down.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nexotrack/Core/theme/color.dart';
 import 'package:nexotrack/Models/ApiModel.dart';
 
 class Drop extends StatelessWidget {
@@ -16,10 +17,45 @@ class Drop extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DropdownButtonFormField<CryptoModel>(
+      dropdownColor: PrimaryColor.BckColor,
+      decoration: InputDecoration(
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: SecColor.borderclr),
+          borderRadius: BorderRadius.circular(9),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: SecColor.borderclr),
+          borderRadius: BorderRadius.circular(9),
+        ),
+      ),
+
+      hint: Text("Select Coin", style: TextStyle(color: SecColor.textwhclr)),
+
       value: selectedcoin,
+
       items: coins.map((coin) {
-        return DropdownMenuItem(value: coin, child: Text(coin.name));
+        return DropdownMenuItem<CryptoModel>(
+          value: coin,
+
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(9),
+              // color: PrimaryColor.CrdColor,
+            ),
+            child: Row(
+              children: [
+                Text(
+                  coin.name.toUpperCase(),
+                  style: TextStyle(color: SecColor.textwhclr),
+                ),
+              ],
+            ),
+          ),
+        );
       }).toList(),
+
       onChanged: onChanged,
     );
   }
--- nexotrack/lib/Screnes/AddCoin.dart
+++ nexotrack/lib/Screnes/AddCoin.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:nexotrack/Core/theme/color.dart';
+import 'package:nexotrack/Core/widgets/drop_down.dart';
 import 'package:nexotrack/Core/widgets/text.dart';
+import 'package:nexotrack/Models/ApiModel.dart';
+import 'package:nexotrack/Provider/ApiPro.dart';
+import 'package:provider/provider.dart';
 
-class Addcoin extends StatelessWidget {
+class Addcoin extends StatefulWidget {
   const Addcoin({super.key});
 
+  @override
+  State<Addcoin> createState() => _AddcoinState();
+}
+
+class _AddcoinState extends State<Addcoin> {
   @override
   Widget build(BuildContext context) {
     double w = MediaQuery.of(context).size.width;
     double h = MediaQuery.of(context).size.height;
+    var pro = context.watch<CryptoPro>();
+    CryptoModel? selcoin;
     return Scaffold(
       backgroundColor: PrimaryColor.BckColor,
       body: Container(
@@ -36,7 +47,15 @@ class Addcoin extends StatelessWidget {
                     txt: "Select Cyyptocurrency",
                     FontWeight: FontWeight.w700,
                   ),
-                  TextField(),
+                  Drop(
+                    selectedcoin: selcoin,
+                    onChanged: (value) {
+                      setState(() {
+                        selcoin = value;
+                      });
+                    },
+                    coins: pro.Coinslst,
+                  ),
                   TextField(),
                 ],
               ),
