4bb827409d34e532f7a1a91ae394b66b1d123485
ashir227 <153205690+ashir227@users.noreply.github.com>
2026-05-31 00:20:11 +0500

    Add logic

--- nexotrack/lib/Provider/FunctionsPro.dart
+++ nexotrack/lib/Provider/FunctionsPro.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:nexotrack/Models/portfolio_model.dart';
 import 'package:nexotrack/Screnes/AddCoin.dart';
 
 class FuncPro extends ChangeNotifier {
-  addcoinpro(Addcoin) {
+  addcoinpro(PortfolioModel Addcoin) {
     var box = Hive.box("mycoin");
     box.add(Addcoin);
     notifyListeners();
   }
 
-  TotalAmnt() {
-    var box = Hive.box("Amount");
+  TotalAmnt(PortfolioModel Addcoin) {
+    var totalamnt = Addcoin.qty + Addcoin.buyPrice;
+
+    notifyListeners();
     // final total =
   }
 }
