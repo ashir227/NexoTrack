b28d30fb47d6e335d9e074aeb149931fbab17a11
ashir227 <153205690+ashir227@users.noreply.github.com>
2026-05-30 23:24:05 +0500

    Add Adapter

--- nexotrack/lib/Models/portfolio_model.dart
+++ nexotrack/lib/Models/portfolio_model.dart
@@ -1,6 +1,16 @@
+import 'package:hive/hive.dart';
+
+part 'portfolio_model.g.dart';
+
+@HiveType(typeId: 0)
 class PortfolioModel {
+  @HiveField(0)
   final String name;
+
+  @HiveField(1)
   final double qty;
+
+  @HiveField(2)
   final double buyPrice;
 
   PortfolioModel({
---
+++ nexotrack/lib/Models/portfolio_model.g.dart
@@ -0,0 +1,47 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'portfolio_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class PortfolioModelAdapter extends TypeAdapter<PortfolioModel> {
+  @override
+  final int typeId = 0;
+
+  @override
+  PortfolioModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return PortfolioModel(
+      name: fields[0] as String,
+      qty: fields[1] as double,
+      buyPrice: fields[2] as double,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, PortfolioModel obj) {
+    writer
+      ..writeByte(3)
+      ..writeByte(0)
+      ..write(obj.name)
+      ..writeByte(1)
+      ..write(obj.qty)
+      ..writeByte(2)
+      ..write(obj.buyPrice);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is PortfolioModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
