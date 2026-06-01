3fa00430be0726dfd9adcf75e0f9521d687aea10
ashir227 <153205690+ashir227@users.noreply.github.com>
2026-06-01 22:57:44 +0500

    Coin price %

--- nexotrack/lib/Provider/FunctionsPro.dart
+++ nexotrack/lib/Provider/FunctionsPro.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:nexotrack/Models/ApiModel.dart';
 import 'package:nexotrack/Models/portfolio_model.dart';
 
 class FuncPro extends ChangeNotifier {
   List<PortfolioModel> MyCoin = [];
+  late PortfolioModel Addcoin;
+  late CryptoModel itemapi;
   addcoinpro(PortfolioModel Addcoin) {
     var box = Hive.box("mycoin");
     box.add(Addcoin);
     MyCoin = box.values.cast<PortfolioModel>().toList();
     notifyListeners();
     return null;
+  }
+
+  currentpercent() {
+    var percent = ((itemapi.price - Addcoin.buyPrice) / Addcoin.buyPrice) * 100;
+    return percent;
   }
 }
