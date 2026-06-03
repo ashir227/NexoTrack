f194d8d4a0af7c51357d784dfec7cbb8fb7419a7
ashir227 <153205690+ashir227@users.noreply.github.com>
2026-06-03 20:26:27 +0500

    Coin Details

--- nexotrack/lib/Screnes/coindetail.dart
+++ nexotrack/lib/Screnes/coindetail.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nexotrack/Core/theme/color.dart';
+import 'package:nexotrack/Core/widgets/text.dart';
 
 class coindetails extends StatelessWidget {
   const coindetails({super.key});
@@ -8,26 +9,32 @@ class coindetails extends StatelessWidget {
   Widget build(BuildContext context) {
     final w = MediaQuery.of(context).size.width;
     final h = MediaQuery.of(context).size.height;
-    return Scaffold(
-      backgroundColor: PrimaryColor.BckColor,
-      body: Column(
-        children: [
-          Card(
-            shadowColor: SecColor.borderclr,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16),
-            ),
-            elevation: 7,
 
-            margin: EdgeInsets.symmetric(
-              horizontal: w * 0.05,
-              vertical: h * 0.02,
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(horizontal: w * 0.05, vertical: h * 0.05),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/images/c_logo.jpg",
+              height: h * 0.25,
+              width: w * 0.45,
             ),
-            color: PrimaryColor.CrdColor,
-            // margin: EdgeInsets.all(w * 0.05),
-            child: Padding(padding: EdgeInsets.all(w * 0.9), child: Column()),
-          ),
-        ],
+
+            SizedBox(height: h * 0.02),
+
+            reusetext(
+              context: context,
+              clr: SecColor.borderclr,
+              Size: 20,
+              txt: "txt",
+              FontWeight: FontWeight.w800,
+            ),
+          ],
+        ),
       ),
     );
   }
